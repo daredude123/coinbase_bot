817529d15c721a08037d04ec27aa217e50ee7b84
andreas <andsek@hotmail.com>
2024-04-07 23:43:32 +0200

    Adding domain and some edit to the client

--- client/CoinbaseClient.cs
+++ client/CoinbaseClient.cs
@@ -1,16 +1,27 @@
+using coinbase_bot.domain;
+using Newtonsoft.Json;
+
 namespace coinbase_bot.client;
 
 public class CoinbaseClient
 {
 
-
-    public string getCurrentPrice(string currencyPair)
+    string coinbaseClient = "https://api.coinbase.com";//"/v2/prices/BTC-USD/buy";
+    private static HttpClient sharedClient = new()
     {
+        BaseAddress = new Uri("https://api.coinbase.com")
+    };
 
+    public async Task<BtcNokPrice> getCurrentPrice(string currencyPair)
+    {
+        string json = await callCoinbase("/v2/prices/BTC-NOK/BUY", HttpMethod.Get);
+        return JsonConvert.DeserializeObject<BtcNokPrice>(json);
     }
 
-    public string callCoinbase(string url, HttpMethod method)
+    public async Task<string> callCoinbase(string url, HttpMethod method)
     {
-        string timestamp = DateTime.Now.ToString();
-        string message = methode
+        HttpResponseMessage response = await sharedClient.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+
     }
+}
