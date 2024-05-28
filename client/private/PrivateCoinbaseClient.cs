b197df98265205d8a24656e579f68690ae438545
andreas <andsek@hotmail.com>
2024-05-28 23:53:17 +0200

    adding some feat changes, missing orgname and key

--- client/private/PrivateCoinbaseClient.cs
+++ client/private/PrivateCoinbaseClient.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
 using coinbase_bot.authorization;
 
 namespace coinbase_bot.client;
 
-public class PrivateCoinbaseClient(IAuthorize authorize, ILogger<PrivateCoinbaseClient> logger) : IPrivateCoinbaseClient
+public class PrivateCoinbaseClient(IAuthorize authorize, ILogger<PrivateCoinbaseClient> logger)
+    : IPrivateCoinbaseClient
 {
     private readonly IAuthorize _authorize = authorize;
     private readonly string OrgName;
@@ -15,19 +15,17 @@ public class PrivateCoinbaseClient(IAuthorize authorize, ILogger<PrivateCoinbase
 
     public string ListAccounts()
     {
-
-
+        return "";
     }
 
-
-    private static async Task<string> CallCoinbase(string url, HttpMethod method)
+    private async Task<string> CallCoinbase(string url, HttpMethod method)
     {
-        string jwt = getJWT(url);
+        string jwt = getJWT(url, method);
         HttpResponseMessage response = await sharedClient.GetAsync(url);
         return await response.Content.ReadAsStringAsync();
     }
 
-    private string getJWT(string url)
+    private string getJWT(string url, HttpMethod method)
     {
         Header header = new Header();
         header.Alg = "ES256";
@@ -35,19 +33,27 @@ public class PrivateCoinbaseClient(IAuthorize authorize, ILogger<PrivateCoinbase
         header.Kid = OrgName;
         header.Nonce = getEpoch();
 
+        string uri  = method.Method + " " + url;
 
-        Dictionary<string, object> claims = new Dictionary<string,object>();
+        Dictionary<string, object> claims = new Dictionary<string, object>();
+        claims.Add("iss", "cdp");
+        claims.Add("nbf", getEpoch());
+        claims.Add("exp", getEpoch(120));
+        claims.Add("sub", OrgName);
+        claims.Add("uri", uri);
+
         Payload payload = new Payload();
         payload.Claims = claims;
-       string jwt = _authorize.MakeJwt(header, payload, secretKey: key);
+        string jwt = _authorize.MakeJwt(header, payload, secretKey: key);
 
         return jwt;
     }
 
-    private string getEpoch()
+    private string getEpoch(int v = 0)
     {
         TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
         int secondsSinceEpoch = (int)t.TotalSeconds;
+        secondsSinceEpoch += v;
         return secondsSinceEpoch.ToString();
     }
 }
